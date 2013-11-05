c4ff1c9453a532fef18d96d79bb067b450cb0edf
Christopher Foo <chris.foo@gmail.com>
2013-11-05 12:34:58 +0800

    Stub in pagination

--- hyves.lua
+++ hyves.lua
@@ -2,23 +2,90 @@ local url_count = 0
 local new_url_count = 0
 
 read_file = function(file)
-    if file then
-        local f = io.open(file)
-        local data = f:read("*all")
-        f:close()
-        return data
-    else
-        return ""
-    end
+  if file then
+    local f = io.open(file)
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+-- range(a) returns an iterator from 1 to a (step = 1)
+-- range(a, b) returns an iterator from a to b (step = 1)
+-- range(a, b, step) returns an iterator from a to b, counting by step.
+-- http://lua-users.org/wiki/RangeIterator
+function range(a, b, step)
+  if not b then
+    b = a
+    a = 1
+  end
+  step = step or 1
+  local f =
+  step > 0 and
+  function(_, lastvalue)
+    local nextvalue = lastvalue + step
+    if nextvalue <= b then return nextvalue end
+  end or
+  step < 0 and
+  function(_, lastvalue)
+    local nextvalue = lastvalue + step
+    if nextvalue >= b then return nextvalue end
+  end or
+  function(_, lastvalue) return lastvalue end
+  return f, nil, a - step
+end
+
+add_urls_from_pager = function(html, urls, hostname)
+  local name = string.match(html, "name: '([^']+)'")
+  local num_pages = to_number(html, string.match("nrPages: ([0-9]+)"))
+  local extra = string.match(html, "extra: '([^']+)'")
+
+  for page_number in range(1, num_pages) do
+
+    local fields = {}
+
+    fields["pageNr"] = page_number
+    fields["config"] = "hyvespager-config.php"
+    fields["showReadMoreLinks"] = "false"
+    fields["extra"] = extra
+
+    table.insert(urls, {
+      url="http://"..hostname.."/index.php?xmlHttp=1&module=pager&action=showPage&name="..name,
+      post_data=cgilua.urlcode.encodetable(fields)
+    })
+
+    new_url_count = new_url_count + 1
+  end
 end
 
 wget.callbacks.get_urls = function(file, url, is_css, iri)
-    -- progress message
-    url_count = url_count + 1
-    if url_count % 5 == 0 then
-        io.stdout:write("\r - Downloaded "..url_count.." URLs. Discovered "..new_url_count.." URLs")
-        io.stdout:flush()
-    end
-    
-    -- TODO: paging stuff
+  -- progress message
+  url_count = url_count + 1
+  if url_count % 5 == 0 then
+    io.stdout:write("\r - Downloaded "..url_count.." URLs. Discovered "..new_url_count.." URLs")
+    io.stdout:flush()
+  end
+
+
+  local urls = {}
+  local html = nil
+  local hostname = string.match(url, "http://([^/]+)")
+
+  if string.match(url, "hyves.nl/friends/") then
+    -- paginate the friends (quickfinder_member_friends)
+    html = read_file(file)
+    add_urls_from_pager(html, urls, hostname)
+  elseif string.match(url, "hyves.nl/fotos/") or string.match(url, "hyves.nl/photos/") then
+    -- paginate the photos (albumlistwithpreview)
+    html = read_file(file)
+    add_urls_from_pager(html, urls, hostname)
+  end
+
+  -- TODO: paginate other stuff
+  -- TODO: check throttle
+  -- TODO: check if wget will parse out the urls from the html fragment from the pagination request
+  -- TODO: grab photos
 end
+
